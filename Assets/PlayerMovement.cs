60e388290737d049474f6bd7304951e8bd3b27c1
TrombiSE <38830192+TrombiSE@users.noreply.github.com>
2019-12-06 23:35:18 +0600

    Add Collision in game

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,27 +6,43 @@ public class PlayerMovement : MonoBehaviour
 
     public float Forward = 300f;
     public float Side = 300f;
+    public float Jump = 1000f;
 
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello WORld!");
-
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKey("w"))
+        {
         rb.AddForce(0, 0, Forward * Time.deltaTime);
 
-        if (Input.GetKey("d")) {
+        }
+
+        if (Input.GetKey("d")) 
+        {
             rb.AddForce(Side * Time.deltaTime, 0, 0);
 
         }
         if (Input.GetKey("a"))
         {
             rb.AddForce(-Side * Time.deltaTime, 0, 0);
-        }   
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -Forward * Time.deltaTime);
+        }
+
+        if (Input.GetKey("space")){
+            rb.AddForce(0, Jump * Time.deltaTime, 0);
+
+        }
+
     }
 }
