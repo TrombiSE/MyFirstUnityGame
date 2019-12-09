3ca766ee50a6fe931b90e9e295e97a9cc8b94321
TrombiSE <38830192+TrombiSE@users.noreply.github.com>
2019-12-09 21:50:36 +0600

    Added score and some level design

---
+++ Assets/ScriptForText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptForText : MonoBehaviour
+{
+    public Transform player;
+    public Text ScoreText;
+
+
+    void Update()
+    {
+
+        ScoreText.text = player.position.z.ToString("0");
+
+    }
+}
